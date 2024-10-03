3381881d06ccc5b36e949466ca26ba2c6b873957
Christian Cruz <seseko98@gmail.com>
2024-10-02 22:55:20 -0500

    config updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://christianc521.github.io/',
-    base: 'personal-website',
+    site: 'https://christiancruz.dev/',
     integrations: [mdx(), sitemap(), tailwind()],
 });
